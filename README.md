ca9af80ad96e4bacb034116a29b8bda540d80114
Roberto Teixeira <r@robteix.com>
2011-12-01 14:14:21 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+json2type
+=========
+
+Convert a json response into a Go type-struct.
+
+
---
+++ json2type.go
@@ -0,0 +1,73 @@
+// json2type - convert json response into a Go type-struct
+//
+// Copyright 2011 The json2type Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
+// This is only a helping tool... do not trust it too much
+// useful to run from inside Emacs with C-u M-|
+package main
+
+import (
+	"json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"os"
+)
+
+func camel(s string) string {
+	var news string
+	for _, w := range strings.Split(s, "_") {
+		news += strings.ToUpper(w[0:1]) + w[1:]
+	}
+	return news
+}
+
+// reads from std input and dumps to std out
+func main() {
+	buf, _ := ioutil.ReadAll(os.Stdin)
+	var obj interface{}
+	json.Unmarshal(buf, &obj)
+	fmt.Println("type MyType ")
+	switch v := obj.(type) {
+	case []interface{}:
+		gen(v[0], "")
+	default:
+		gen(obj, "")
+	}
+}
+
+func jname(n string) string {
+	if n == "" {
+		return "\n"
+	}
+	return fmt.Sprintf("`json:\"%s\"`\n", n)
+}
+func gen(obj interface{}, name string) {
+	switch v := obj.(type) {
+	case float64:
+		fmt.Printf("int64 %s\n", jname(name))
+	case []interface{}:
+		fmt.Print("[]")
+		if len(v) < 1 {
+			fmt.Printf("string %s", jname(name))
+		} else {
+			gen(v[0], name)
+		}
+	case map[string]interface{}:
+		fmt.Printf("struct {")
+		for k, i := range v {
+			fmt.Printf("%s ", camel(k))
+			gen(i, k)
+		}
+		fmt.Printf("} %s", jname(name))
+	case string:
+		fmt.Printf("string %s", jname(name))
+	case bool:
+		fmt.Printf("bool %s", jname(name))
+	default:
+		fmt.Printf("string %s", jname(name))
+	}
+}
